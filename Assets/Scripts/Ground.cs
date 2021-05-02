ab523413b7934e31e38d3260f57bcd7e67b0331f
NICK-PC\Nick <smertnick@gmail.com>
2021-05-02 21:48:30 +0300

    Ground swap works

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -5,4 +5,28 @@ public class Ground : MonoBehaviour
 {
     [SerializeField] private GroundController ground;
     [SerializeField] private Tilemap tmap;
+    private Theme theme = Theme.Happy;
+
+    private void Start()
+    {
+        theme = GameManager.Instance.theme;
+        Events.OnThemeChange += HandleThemeChange;
+    }
+
+    private void HandleThemeChange(Theme newTheme)
+    {
+        for (int i = 0; i < ground.Sets[(int)theme].Tiles.Length; i++)
+        {
+            var tileFrom = ground.Sets[(int) theme].Tiles[i];
+            var tileTo = ground.Sets[(int) newTheme].Tiles[i];
+            if (tileTo != tileFrom)
+                tmap.SwapTile(tileFrom, tileTo);
+        }
+        theme = newTheme;
+    }
+
+    private void OnDestroy()
+    {
+        Events.OnThemeChange -= HandleThemeChange;
+    }
 }
