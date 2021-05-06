0a8fc87fad03551f58e510134eed1b9d7d5a5b9c
NICK-PC\Nick <smertnick@gmail.com>
2021-05-06 15:10:50 +0300

    added brutal bullet

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,11 +6,24 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private Rigidbody2D rbody;
+    private SpriteRenderer spriteRenderer;
+    private Theme theme = Theme.Happy;
+    [SerializeField] private Sprite[] sprites;
     [SerializeField] private float force = 300f;
 
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        theme = GameManager.Instance.theme;
+        spriteRenderer.sprite = sprites[(int) theme];
+        Events.OnThemeChange += HandleThemeChange;
+    }
+
+    private void HandleThemeChange(Theme newTheme)
+    {
+        theme = newTheme;
+        spriteRenderer.sprite = sprites[(int) theme];
     }
 
     private void OnEnable()
@@ -24,5 +37,10 @@ public class Projectile : MonoBehaviour
         gameObject.SetActive(false);
         if (other.gameObject.GetComponent<IDamageable>() == null) return;
         other.gameObject.GetComponent<IDamageable>().TakeDamage(1);
+    }
+
+    private void OnDestroy()
+    {
+        Events.OnThemeChange -= HandleThemeChange;
     }
 }
