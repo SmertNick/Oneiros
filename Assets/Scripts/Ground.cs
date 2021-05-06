362cc438aadc615696f75b6eb51856fd591e3274
NICK-PC\Nick <smertnick@gmail.com>
2021-05-06 22:14:34 +0300

    Tilemap Swap optimized (created second tilemap and enable/disable them instead of tileswap everytime)

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -7,25 +7,46 @@ public class Ground : MonoBehaviour
     [SerializeField] private GroundController ground;
     [SerializeField] private Tilemap tmap;
     private Theme theme = Theme.Happy;
+    private Tilemap tmap2;
+    
 
     private void Start()
     {
         theme = GameManager.Instance.theme;
         Events.OnThemeChange += HandleThemeChange;
+        SetUpTileMaps();
+    }
+
+
+    private void SetUpTileMaps()
+    {
+        var groundObj = tmap.gameObject;
+        tmap2 = Instantiate(groundObj, transform.position, Quaternion.identity, groundObj.transform.parent).GetComponent<Tilemap>();
+        tmap2.gameObject.SetActive(false);
+        for (int i = 0; i < ground.Sets[0].Tiles.Length; i++)
+        {
+            var tileFrom = ground.Sets[0].Tiles[i];
+            var tileTo = ground.Sets[1].Tiles[i];
+            if (tileTo != tileFrom)
+                tmap2.SwapTile(tileFrom, tileTo);
+        }
     }
 
     private void HandleThemeChange(Theme newTheme)
     {
-        for (int i = 0; i < ground.Sets[(int)theme].Tiles.Length; i++)
+        if (newTheme == Theme.Brutal)
         {
-            var tileFrom = ground.Sets[(int) theme].Tiles[i];
-            var tileTo = ground.Sets[(int) newTheme].Tiles[i];
-            if (tileTo != tileFrom)
-                tmap.SwapTile(tileFrom, tileTo);
+            tmap.gameObject.SetActive(false);
+            tmap2.gameObject.SetActive(true);
+        }
+        else
+        {
+            tmap2.gameObject.SetActive(false);
+            tmap.gameObject.SetActive(true);
         }
         theme = newTheme;
     }
-
+    
     private void OnDestroy()
     {
         Events.OnThemeChange -= HandleThemeChange;
