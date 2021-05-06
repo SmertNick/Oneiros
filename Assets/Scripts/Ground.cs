db0a3fb13ecc1e48be8582f01e175b36b9e12a7a
NICK-PC\Nick <smertnick@gmail.com>
2021-05-06 19:43:21 +0300

    removed tilemap collider switch on theme swap

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -7,18 +7,15 @@ public class Ground : MonoBehaviour
     [SerializeField] private GroundController ground;
     [SerializeField] private Tilemap tmap;
     private Theme theme = Theme.Happy;
-    private TilemapCollider2D coll;
 
     private void Start()
     {
         theme = GameManager.Instance.theme;
         Events.OnThemeChange += HandleThemeChange;
-        coll = tmap.gameObject.GetComponent<TilemapCollider2D>();
     }
 
     private void HandleThemeChange(Theme newTheme)
     {
-        coll.enabled = false;
         for (int i = 0; i < ground.Sets[(int)theme].Tiles.Length; i++)
         {
             var tileFrom = ground.Sets[(int) theme].Tiles[i];
@@ -27,7 +24,6 @@ public class Ground : MonoBehaviour
                 tmap.SwapTile(tileFrom, tileTo);
         }
         theme = newTheme;
-        coll.enabled = true;
     }
 
     private void OnDestroy()
