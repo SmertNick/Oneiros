d55f444f7817ed64e5cc61e4a75a4533bc2088db
NICK-PC\Nick <smertnick@gmail.com>
2021-05-08 16:27:05 +0300

    created Pause menu prefab

---
+++ Assets/Scripts/UI and Sound/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private Button resumeButton, optionsButton, restartButton, quitButton;
+    [SerializeField] private GameObject pauseMenu, optionsMenu;
+
+    private void Start()
+    {
+        Events.OnGameStateChange += HandleStateChange;
+        resumeButton.onClick.AddListener(ResumeGame);
+        optionsButton.onClick.AddListener(Options);
+        restartButton.onClick.AddListener(Restart);
+        quitButton.onClick.AddListener(Quit);
+
+        optionsMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+
+    private void HandleStateChange(GameState newState, GameState previousState)
+    {
+        optionsMenu.SetActive(false);
+        pauseMenu.SetActive(newState == GameState.Paused);
+    }
+
+    private void ResumeGame()
+    {
+        GameManager.Instance.TogglePause();
+    }
+
+    private void Options()
+    {
+        optionsMenu.SetActive(!optionsMenu.activeInHierarchy);
+    }
+
+    private void Restart()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Quit()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDestroy()
+    {
+        Events.OnGameStateChange -= HandleStateChange;
+        resumeButton.onClick.RemoveListener(ResumeGame);
+        optionsButton.onClick.RemoveListener(Options);
+        restartButton.onClick.RemoveListener(Restart);
+        quitButton.onClick.RemoveListener(Quit);
+    }
+}
