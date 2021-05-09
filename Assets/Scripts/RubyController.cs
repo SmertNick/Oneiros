dcbcebe3747c53b5e26e5e1d1cf1802d2981ba79
NICK-PC\Nick <smertnick@gmail.com>
2021-05-09 16:30:48 +0300

    Added todo

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+//TODO refactor into separate behaviours
 public class RubyController : MonoBehaviour
 {
     [SerializeField] private RubyStats stats;
